29d4e31e07f577cb073b17dd5f29f8c17acdbc3b
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-03-31 19:56:14 -0400

    api validation

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const winston = require('winston')
 
 const app = express()
 
@@ -15,7 +16,48 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
-app.get('/', (req, res) => {
+// set up winston
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+      new winston.transports.File({ filename: 'info.log' })
+    ]
+});
+  
+if (NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+      format: winston.format.simple()
+    }));
+}
+
+// API handling middleware
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+  
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+      return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    next()
+})
+
+const bookmarks = [
+    {
+        title: 'github',
+        url: 'https://github.com/',
+        description: 'link to github',
+        rating: '5'
+    },
+    {
+        title: 'google',
+        url: 'https://google.com/',
+        description: 'link to google',
+        rating: '4'
+    }
+]
+
+app.get('/bookmarks', (req, res) => {
     res.send('Hello world')
 })
 
@@ -31,3 +73,15 @@ app.use(function errorHandler(error, req, res, next) {
 })
 
 module.exports = app
+
+// Write a route handler for the endpoint GET /bookmarks 
+    // that returns a list of bookmarks
+        //title, url, description, rating 
+// Write a route handler for the endpoint GET /bookmarks/:id 
+    // that returns a single bookmark with the given ID, 
+    // return 404 Not Found if the ID is not valid
+// Write a route handler for POST /bookmarks 
+    // that accepts a JSON object representing a bookmark 
+    // and adds it to the list of bookmarks after validation.
+// Write a route handler for the endpoint DELETE /bookmarks/:id 
+    // that deletes the bookmark with the given ID.
