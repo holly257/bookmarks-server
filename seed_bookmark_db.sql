a66327d21b4ecae4281b82c1c70a2d28c5bd81de
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-04-13 14:33:08 -0400

    made first migrations

---
+++ seed_bookmark_db.sql
@@ -0,0 +1,13 @@
+ INSERT INTO bookmarks_list (title, url, description, rating) VALUES 
+    ('Google', 'https://www.google.com/', 'Google homepage', '4'),
+    ('Thinkful', 'https://www.thinkful.com/', 'Thinkful homepage', '3'),
+    ('Twitter', 'https://twitter.com/explore', 'Twitter homepage', '4'),
+    ('Facebook', 'https://www.facebook.com/', 'Facebook homepage', '3'),
+    ('Google Mail', 'https://www.gmail.com/', 'Google Mail homepage', '2'),
+    ('Instagram', 'https://www.instagram.com/', 'Instagram homepage', '5'),
+    ('Wikipedia', 'https://en.wikipedia.org/wiki/Main_Page', 'Wikipedia homepage', '1'),
+    ('Github', 'https://github.com/', 'Github homepage', '3'),
+    ('Apple', 'https://www.apple.com/', 'Apple homepage', '3'),
+    ('Wiki Coronavirus', 'https://en.wikipedia.org/wiki/Coronavirus', 'Coronavirus Wiki homepage', '4'),
+    ('Youtube', 'https://www.youtube.com/', 'Youtube homepage', '5')
+;
